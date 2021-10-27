928ab92ab6394d270f8ed07b5afde31eba319b51
yCyanxK <92628965+yCyanxK@users.noreply.github.com>
2021-10-27 11:28:30 +0800

    virus check
    e

--- Jigsaw 0.26/Jigsaw 0.26 (1.8.8)/Start.java
+++ Jigsaw 0.26/Jigsaw 0.26 (1.8.8)/Start.java
@@ -5,7 +5,7 @@ import net.minecraft.client.main.Main;
 public class Start {
 	public static void main(String[] args) {
 		Main.main(concat(new String[] { "--version", "mcp", "--accessToken", "0", "--assetsDir", "assets",
-				"--assetIndex", "1.8", "--userProperties", "{}" }, args));
+				"--assetIndex", "1.8.8", "--userProperties", "{}" }, args));
 	}
 
 	public static <T> T[] concat(T[] first, T[] second) {
